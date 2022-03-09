520d720512d654bf4fc37da000263230d2e73504
manideep-karumanchi <manideep.karumanchi@gmail.com>
2022-03-09 14:20:36 +0530

    Add files via upload

---
+++ TracingExample/TracingExample/Default.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Trace="true" Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TracingExample.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button Text="Submit" ID="cmdsubmit" runat="server" OnClick="cmdsubmit_Click" />
+        <hr />
+    </div>
+    </form>
+</body>
+</html>
---
+++ TracingExample/TracingExample/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TracingExample
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        
+        }
+
+        protected void cmdsubmit_Click(object sender, EventArgs e)
+        {
+            Trace.Warn("cmdsubmit_click","About to place a value in session");
+            Session["a"] = 100;
+            Trace.Warn("cmdsubmit_click","Placed a value in session state");
+        }
+    }
+}
